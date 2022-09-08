21d36fc3d3cdf76b472dcb242bed676f28911b72
chanjamins <jamin7597@163.com>
2022-09-08 14:49:17 +0800

    这是个项目雏形

---
+++ puki-product/src/main/java/com/mhpk/puki/product/PukiProductApplication.java
@@ -0,0 +1,13 @@
+package com.mhpk.puki.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PukiProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PukiProductApplication.class, args);
+    }
+
+}
